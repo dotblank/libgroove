9728f49cd8472e9294d010dc97c37fbfc1a02cfb
Robin Burchell <robin.burchell@collabora.co.uk>
2010-07-31 04:42:21 +0100

    On connection, verify PHP session ID and session token for both presence and validity.

--- tests/tst_GrooveClient/main.cpp
+++ tests/tst_GrooveClient/main.cpp
@@ -1,6 +1,7 @@
 #include <QtTest>
 
 #include "grooveclient.h"
+#include "grooveclient_p.h"
 
 #include "main.h"
 
@@ -25,7 +26,36 @@ void tst_GrooveClient::establishConnection()
     while (!spy.count() && i != 10) // wait up to 10 seconds for connection, should be plenty
         QTest::qWait(1000);
 
+    // did we connect?
     QVERIFY(spy.count() == 1);
+
+    // check PHP cookie and session token for validity and presence
+    QVERIFY(GrooveClientPrivate::instance()->phpCookie().length() == 32);
+    QVERIFY(GrooveClientPrivate::instance()->sessionToken().length() == 13);
+
+    foreach (const QChar &schar, GrooveClientPrivate::instance()->phpCookie()) {
+        switch (schar.toLatin1()) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':            break;
+        default:
+            qDebug() << "Bad hex character " << schar;
+            QVERIFY(false);
+        }
+    }
+
+    foreach (const QChar &schar, GrooveClientPrivate::instance()->sessionToken()) {
+        switch (schar.toLatin1()) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+            break;
+        default:
+            qDebug() << "Bad hex character " << schar;
+            QVERIFY(false);
+        }
+    }
+
 }
 
 QTEST_MAIN(tst_GrooveClient)
