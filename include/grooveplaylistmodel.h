80dd6596aaebf23846e4f110dbb56e0bf3d53cab
Robin Burchell <robin.burchell@collabora.co.uk>
2010-08-01 02:28:16 +0100

    Relicense for public release.

--- include/grooveplaylistmodel.h
+++ include/grooveplaylistmodel.h
@@ -1,16 +1,18 @@
 /*
- * This file is part of libgroove
+ * Copyright (C) 2010 Robin Burchell <robin.burchell@collabora.co.uk>
  *
- * Copyright (C) 2010 Robin Burchell <viroteck@viroteck.net>
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
  *
- * This software, including documentation, is protected by copyright.
- * All rights are reserved.
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
  *
- * Copying, including reproducing, storing, adapting or translating, any or
- * all of this material requires prior written consent of the author.
- *
- * This material may also contain confidential information which may not be
- * disclosed to others without the prior written consent of the author.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef GROOVEPLAYLISTMODEL_H
